b64e498e818f890db98521a963b59727bcecf917
Omkar141998 <onagpure688@gmail.com>
2023-02-17 21:44:26 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+var inputElement = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var olElement = document.getElementById("todoList");
+
+function addToDoElement (){
+    if(inputElement.value){
+        //we need to create li in after the ol element
+    var li = document.createElement('li');
+    li.textContent = inputElement.value;
+    //add li as a child of ol element
+    olElement.appendChild(li);
+    inputElement.value = "";
+    }
+}
+// cal;l function
+btn.addEventListener("click",addToDoElement);
 
